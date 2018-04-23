9fa8b6bd4b47306665a6389ae74ee818fa6cfc61
beata <bgajewska@sointeractive.pl>
2018-04-23 13:18:33 +0200

    Pierwszy prosty program.

--- src/ru/stqa/pft/sandbox/Main.java
+++ src/ru/stqa/pft/sandbox/Main.java
@@ -4,43 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        hello("world");
-        hello("user");
-
-
-        double l =5;
-
-
-        System.out.println("Powierzchnia kwadratu   " + l + " = " + area(l) );
-
-
-        double a = 4;
-        double b = 6;
-
-        System.out.println("Powierzchnia prostokata o bokach  a+b " + a + " = " +area(a,b) );
+        System.out.println("Hello World!");
 
     }
-    public static void hello(String somebody){
-
-        System.out.println("Hello,  "+ somebody + "!");
-
-
     }
 
-    public static double  area(double len){
-   return len*len;
-
-
-
-
-    }
-
-    public static double area (double a , double b ){
-    return a * b;
-
-
-
-    }
-
-
-}
