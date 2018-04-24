1e61ba400d445ab430cf01c9b8d9357b9343caa3
beata <bgajewska@sointeractive.pl>
2018-04-24 15:00:24 +0200

    Zadanie 1 -  do poprawy.

--- src/ru/stqa/pft/sandbox/Point.java
+++ src/ru/stqa/pft/sandbox/Point.java
@@ -2,17 +2,33 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public static void main(String[] args) {}
+
+  public static void main(String[] args) {
 
 
-  public double x ;
-  public double y ;
+    private double x;
+    private double y;
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
+  public Point( double x, double y){
+      this.x = x;
+      this.y = y;
+    }
   }
+  public static double distance(Point p1, Point p2) {
 
 
+    double dx;
+    double dy;
+    dx = p2.x - p1.x;
+    dy = p2.y - p1.y;
+
+    System.out.println("dlugosc odcinka o współrzędnych" + p1.x + p1.y + " oraz " + p2.x + p2.y + " wynosi " + distance );
+
+    return Math.sqrt((dx * dx) + (dy * dy));
+
+
+
+
+}
 
 }
